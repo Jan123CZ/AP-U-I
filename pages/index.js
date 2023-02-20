8c6461b6cfb8d68f2cd84f49d5ebc5ed8247f426
Jakub Maryška <maryska@spstrutnov.cz>
2023-02-20 09:19:29 +0100

    vytvoření html a css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Head from "next/head";
+
 export default function Home(){
   return (
-    <></>
+    <>
+      <Head>
+        <title>Api web</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <body>
+        <h1>Vítejte na stránce</h1>
+      </body>
+    </>
   )
 }
