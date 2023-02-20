4ef693dfc7bf3951fc0a211e680131f31b2b0161
Jaroslav Semerák <semerak@spstrutnov.cz>
2023-02-20 09:11:26 +0100

    Zkoušky

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,15 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+const api_key = 'IFhrQRA6ISGhLn2yRsaETGlvwiAtT0TN';
+const movie_id = 550;
+const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${api_key}`;
+
+fetch(url)
+  .then(response => response.json())
+  .then(movie_data => {
+    // Display the movie title and overview
+    console.log(`Movie title: ${movie_data.title}`);
+    console.log(`Overview: ${movie_data.overview}`);
+  })
+  .catch(error => console.error('An error occurred while fetching movie data:', error));
